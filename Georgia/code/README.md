b79be1e4bd25b33a0fa2e8d7b5977d7b87274970
hwheelen <hwheelen@berkeley.edu>
2021-01-12 14:14:17 -0500

    adding partisan figures

--- Georgia/code/README.md
+++ Georgia/code/README.md
@@ -1,4 +1,4 @@
-#Data Preperation for Historical Analysis of Gerrymandering in GA
+# Data Preperation for Historical Analysis of Gerrymandering in GA
 
 This folder contains everything you need to take GA district shapefiles 
 (like the ones found [here](https://data-hub.gio.georgia.gov/datasets/GARC::georgia-senate-districts)) 
@@ -6,7 +6,7 @@ and [census blocks](https://catalog.data.gov/dataset/tiger-line-shapefile-2017-2
 plus demographic data and [precinct level election results](https://openprecincts.org/ga/)
 to assess how granular data about people has been used during line drawing. 
 
-##Code
+## Code
 The following is a list of the scripts in the folder /code, by order of use
 - [cenpy_script_GA.py](https://github.com/hwheelen/PGP_RapidResponse/blob/master/Georgia/code/cenpy_script_GA.py)
     Uses python package [cenpy](https://pypi.org/project/cenpy/) to download census data for GA.
@@ -22,7 +22,7 @@ The following is a list of the scripts in the folder /code, by order of use
     Uses demographic information to generate figures showing packing and cracking
 
 
-##Data
+## Data
 The /data folder contains outputs from the /extract_data.py script for all maps from 2011-2020
 These are ready to be used to generate the figures in both generate_figures scripts
 or other analysis or figure generation
--- Georgia/code/make_partisan_graphs_GA.py
+++ Georgia/code/make_partisan_graphs_GA.py
@@ -4,19 +4,22 @@ import matplotlib.pyplot as plt
 plt.style.use('seaborn-whitegrid')
 import numpy as np
 
-#load in datasets
-sen_df= pd.read_csv('/Users/hwheelen/Documents/GitHub/PGP_RapidResponse/Wisconsin/data/WI_State_Senate_Map_estimates.csv')
-pgp_df = pd.read_csv('/Users/hwheelen/Documents/GitHub/PGP_RapidResponse/Wisconsin/data/PGP_WI_Senate_Demo_Map_estimates.csv')
+sen = pd.read_csv('/Users/hwheelen/Documents/GitHub/PGP_RapidResponse/Georgia/Data/GA_State_Senate_Map_estimates.csv')
+sen12 = pd.read_csv('/Users/hwheelen/Documents/GitHub/PGP_RapidResponse/Georgia/Data/GA_State_Senate12_Map_estimates.csv')
+house = pd.read_csv('/Users/hwheelen/Documents/GitHub/PGP_RapidResponse/Georgia/Data/GA_State_House_Map_estimates.csv')
+cong = pd.read_csv('/Users/hwheelen/Documents/GitHub/PGP_RapidResponse/Georgia/Data/GA_Congressional_Map_estimates.csv')
+
+measure = 'G16PREDCli'
 
 
-#calculate y proportion and set, sometimes this might require calculating an average of ma
-x = df['rank']
-y = df['propD']
-n = df['District']
+# senate12 figure-------------------------------------------------------------------
+sen['prop'] = (sen[measure]/(sen['G16PREDCli'] + sen['G16PRERTru'])*100
 
-x = [4,7,9,10,6,11,3]
-seq = sorted(x)
-index = [seq.index(v) for v in x]
+sen['rank'] = sen['prop'].rank(ascending=True)
+
+x = sen['rank']
+y = sen['prop']
+n = sen['DISTRICT']
 
 
 fig, ax = plt.subplots()
@@ -34,16 +37,126 @@ for i in range(len(x)):
     # plotting the corresponding x with y  
     # and respective color 
     ax.scatter(x[i], y[i], c = col[i], s = 30 )
-ax.axhspan(45, 55, alpha=0.25, color='grey')
-ax.set_ylabel('Democratic vote share (%)')
-ax.set_ylim(0,max(y)+2)
+ax.axhspan(45, 55, alpha=0.25, color='yellow')
+ax.set_ylabel('Proportion Democratic Presidental Votes (Clinton 16)')
+ax.set_ylim(-2,max(y)+2)
 ax.set_xlabel('Rank (least to most Democratic)')
 ax.set_xlim(0,max(x)+2)
-ax.axhline(50, color ='black')
-ax.set_title('Wisconsin Senate, Princeton demonstration map')
+#ax.axhline(50, color ='black')
+ax.set_title('Georgia State Senate, Enacted Map')
 #ax.set_title('Wisconsin Senate Map 2011-2020')
-plt.xticks(np.arange(1, 34, step=4))
-plt.yticks(np.arange(0, 105, step=10))
+#plt.xticks(np.arange(1, 34, step=4))
+#plt.yticks(np.arange(0, 105, step=10))
 
-fig.savefig('/Users/hwheelen/Desktop/Wisconsin/Images/PGPWIDemoMap.pdf')
-#fig.savefig('/Users/hwheelen/Desktop/Wisconsin/Images/WIStSenateMap.pdf')
+fig.savefig('/Users/hwheelen/Documents/GitHub/PGP_RapidResponse/Georgia/Figures/GA_Senate_Enacted_Pres16.pdf')
+
+# senate12 figure--------------------------------------------------------------------------------------------------------
+sen12['prop'] = (sen12[measure]/(sen12['G16PREDCli'] + sen12['G16PRERTru'])*100
+
+sen12['rank'] = sen12['prop'].rank(ascending=True)
+
+x = sen12['rank']
+y = sen12['prop']
+n = sen12['DISTRICT']
+
+fig, ax = plt.subplots()
+
+col =[] 
+  
+for i in range(0, len(x)): 
+    if y[i]<50: 
+        col.append('red')   
+    else: 
+        col.append('blue')  
+  
+for i in range(len(x)): 
+      
+    # plotting the corresponding x with y  
+    # and respective color 
+    ax.scatter(x[i], y[i], c = col[i], s = 30 )
+ax.axhspan(45, 55, alpha=0.25, color='yellow')
+ax.set_ylabel('Proportion Democratic Presidental Votes (Clinton 16)')
+ax.set_ylim(-2,max(y)+2)
+ax.set_xlabel('Rank (least to most Democratic)')
+ax.set_xlim(0,max(x)+2)
+#ax.axhline(50, color ='black')
+ax.set_title('Georgia State Senate, 2012 Map')
+#ax.set_title('Wisconsin Senate Map 2011-2020')
+#plt.xticks(np.arange(1, 34, step=4))
+#plt.yticks(np.arange(0, 105, step=10))
+
+fig.savefig('/Users/hwheelen/Documents/GitHub/PGP_RapidResponse/Georgia/Figures/GA_Senate_2012_Pres16.pdf')
+
+# house figure--------------------------------------------------------------------------------------------------------
+house['prop'] = (house[measure]/(house['G16PREDCli'] + house['G16PRERTru'])*100
+
+house['rank'] = house['prop'].rank(ascending=True)
+
+x = house['rank']
+y = house['prop']
+n = house['DISTRICT']
+
+fig, ax = plt.subplots()
+
+col =[] 
+  
+for i in range(0, len(x)): 
+    if y[i]<50: 
+        col.append('red')   
+    else: 
+        col.append('blue')  
+  
+for i in range(len(x)): 
+      
+    # plotting the corresponding x with y  
+    # and respective color 
+    ax.scatter(x[i], y[i], c = col[i], s = 30 )
+ax.axhspan(45, 55, alpha=0.25, color='yellow')
+ax.set_ylabel('Proportion Democratic Presidental Votes (Clinton 16)')
+ax.set_ylim(-2,max(y)+2)
+ax.set_xlabel('Rank (least to most Democratic)')
+ax.set_xlim(0,max(x)+2)
+#ax.axhline(50, color ='black')
+ax.set_title('Georgia State House, Enacted Map')
+#ax.set_title('Wisconsin Senate Map 2011-2020')
+#plt.xticks(np.arange(1, 34, step=4))
+#plt.yticks(np.arange(0, 105, step=10))
+
+fig.savefig('/Users/hwheelen/Documents/GitHub/PGP_RapidResponse/Georgia/Figures/GA_House_Enacted_Pres16.pdf')
+
+#congressional figure--------------------------------------------------------------------------------------------------------
+cong['prop'] = (cong[measure]/(cong['G16PREDCli'] + cong['G16PRERTru'])*100
+
+cong['rank'] = cong['prop'].rank(ascending=True)
+
+x = cong['rank']
+y = cong['prop']
+n = cong['DISTRICT']
+
+fig, ax = plt.subplots()
+
+col =[] 
+  
+for i in range(0, len(x)): 
+    if y[i]<50: 
+        col.append('red')   
+    else: 
+        col.append('blue')  
+  
+for i in range(len(x)): 
+      
+    # plotting the corresponding x with y  
+    # and respective color 
+    ax.scatter(x[i], y[i], c = col[i], s = 30 )
+ax.axhspan(45, 55, alpha=0.25, color='yellow')
+ax.set_ylabel('Proportion Democratic Presidental Votes (Clinton 16)')
+ax.set_ylim(-2,max(y)+2)
+ax.set_xlabel('Rank (least to most Democratic)')
+ax.set_xlim(0,max(x)+2)
+#ax.axhline(50, color ='black')
+ax.set_title('US Congress, Enacted Map')
+#ax.set_title('Wisconsin Senate Map 2011-2020')
+#plt.xticks(np.arange(1, 34, step=4))
+#plt.yticks(np.arange(0, 105, step=10))
+
+fig.savefig('/Users/hwheelen/Documents/GitHub/PGP_RapidResponse/Georgia/Figures/GA_Congressional_Enacted_Pres16.pdf')
